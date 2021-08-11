910be8d4ed9d9266614307637faaab049df44c9b
alstjr5949 <alstjr5949@gmail.com>
2021-08-11 17:23:23 +0900

    if문 간소화, box background 문제해결

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const boxs = document.querySelectorAll(".box");
-const blackArr = ["#eeeeee", "#bdbdbd", "#757575", "#000000"];
-let j = -1;
+const boxs = document.querySelectorAll("#box");
+const blackArr = ["white", "#eeeeee", "#bdbdbd", "#757575", "#000000"];
 
-for(let i =0; i < boxs.length; i++){
-  boxs[i].addEventListener("click", function(){
-    if(j ===  -1){
-      j = j + 1;
-    } else if(j === 0){
-      j = j + 1;
-    } else if(j === 1){
-      j = j + 1;
-    } else if(j === 2){
-      j = j + 1;
-    } else if(j === 3){
-      j = 0;
-    }; 
-      boxs[i].style.backgroundColor = blackArr[j];
-  })
+const boxObjs = [];
+
+for(let i = 0; i < boxs.length; i++){
+  const boxObj = {
+    box : boxs[i],
+    indexNum: 0
+  };
+  boxObjs.push(boxObj);
+}
+
+//기존 if문 간소화, j값을 indexNum으로 대체
+for (let i = 0; i < boxObjs.length; i++){
+  boxObjs[i].box.addEventListener("click",function(){
+    boxObjs[i].indexNum = boxObjs[i].indexNum + 1;
+      if(boxObjs[i].indexNum === 5){
+        boxObjs[i].indexNum = 0;
+      }
+      boxObjs[i].box.style.backgroundColor = blackArr[boxObjs[i].indexNum];
+  });
 }
